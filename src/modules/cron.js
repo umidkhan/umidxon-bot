c63a769df7be94e745a63ccef42c58ca20cb4595
umidkhan <umidxondeveloper@gmail.com>
2024-04-26 15:22:07 +0500

    configurated

--- src/modules/cron.js
+++ src/modules/cron.js
@@ -1,12 +1,12 @@
 const cron = require("node-cron");
 const axios = require("axios");
 
-const task = cron.schedule("* * * * *", async () => {
+const task = cron.schedule("1-10 * * * *", async () => {
 	try {
 		const response = await axios.post(process.env.WEBHOK_URI);
 		console.log(`POST reqest sent: ${response.data}`);
 	} catch (err) {
-		console.error(`~ERROR: ${err}`);
+		console.error(err);
 	}
 });
 
