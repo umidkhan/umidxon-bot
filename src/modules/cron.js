5cd1a5ec982107f965bdc3218bb976d5ca91186f
umidkhan <umidxondeveloper@gmail.com>
2024-04-27 18:12:27 +0500

    fixed

--- src/modules/cron.js
+++ src/modules/cron.js
@@ -1,9 +1,11 @@
 const cron = require("node-cron");
 const axios = require("axios");
 
-const task = cron.schedule("1-10 * * * *", async () => {
+const task = cron.schedule("* * * * *", async () => {
 	try {
-		const response = await axios.post(process.env.WEBHOK_URI);
+		const response = await axios.post(
+			"https://umidxon-bot.onrender.com/telegram"
+		);
 		console.log(`POST reqest sent: ${response.data}`);
 	} catch (err) {
 		console.error(err);
