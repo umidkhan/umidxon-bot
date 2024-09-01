4782526fe13d9706184fdb26ec3554ad9e1b1ebf
umidkhan <umidxondeveloper@gmail.com>
2024-09-01 13:52:04 +0500

    channel post edit feature added

--- src/core/bot.js
+++ src/core/bot.js
@@ -8,123 +8,135 @@ const stage = new Scenes.Stage([anonimScene, senderScene]);
 bot.use(session());
 bot.use(stage.middleware());
 
-bot.start(async ctx => {
-	await ctx.reply(
-		`Assalomu alaykum <b><a href="tg://user?id=${ctx.from.id}" >${ctx.from.first_name}</a></b>\n@umidxon_polatxonov'ga xabar yuborish uchun pastdagi istalgan turni tanlang 👇`,
-		{
-			parse_mode: "HTML",
-			reply_markup: {
-				inline_keyboard: [
-					[
-						{ text: "🛡 Anonim", callback_data: "anonim" },
-						{ text: "👀 Anonim emas", callback_data: "simple" },
-					],
-				],
-				one_time_keyboard: true,
-				resize_keyboard: true,
-			},
-		}
-	);
+bot.start(async (ctx) => {
+  await ctx.reply(
+    `Assalomu alaykum <b><a href="tg://user?id=${ctx.from.id}" >${ctx.from.first_name}</a></b>\n@umidxon_polatxonov'ga xabar yuborish uchun pastdagi istalgan turni tanlang 👇`,
+    {
+      parse_mode: "HTML",
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: "🛡 Anonim", callback_data: "anonim" },
+            { text: "👀 Anonim emas", callback_data: "simple" },
+          ],
+        ],
+        one_time_keyboard: true,
+        resize_keyboard: true,
+      },
+    }
+  );
 });
 
-bot.command("new_message", async ctx => {
-	await ctx.reply("Qaysi turda xabar yubormoqchisiz? 👇", {
-		reply_markup: {
-			inline_keyboard: [
-				[
-					{ text: "🛡 Anonim", callback_data: "anonim" },
-					{ text: "👀 Anonim emas", callback_data: "simple" },
-				],
-			],
-			one_time_keyboard: true,
-			resize_keyboard: true,
-		},
-	});
+bot.on("channel_post", (ctx) => {
+  const message = ctx.update.channel_post;
+
+  ctx.editMessageText(`${message.text}\n\n<b>@umidxon_blog</b>\n\n`, {
+    parse_mode: "HTML",
+  });
+  setTimeout(() => {
+    ctx.react("🆒");
+  }, 30000);
 });
 
-bot.action("anonim", async ctx => {
-	await ctx.scene.enter("anonimScene");
-	ctx.answerCbQuery("Iltimos faqat matnli xabar yuboring");
-	setTimeout(async () => {
-		await ctx.telegram.deleteMessage(ctx.chat.id, ctx.msgId);
-	}, 300);
-});
-bot.action("simple", async ctx => {
-	await ctx.scene.enter("senderScene");
-	ctx.answerCbQuery("Iltimos faqat matnli xabar yuboring");
-	setTimeout(async () => {
-		await ctx.telegram.deleteMessage(ctx.chat.id, ctx.msgId);
-	}, 100);
-});
-
-bot.action("cencel", async ctx => {
-	ctx.reply(
-		"<b>Muvaffaqiyatli bekor qilindi</b>\nYangi xabar yuborish uchun <b>/new_message</b> buyrug'idan foydalanishingiz mumkin",
-		{ parse_mode: "HTML" }
-	);
-	await ctx.scene.leave();
-	ctx.answerCbQuery("Bekor qilindi ✅");
-	await ctx.telegram.deleteMessage(ctx.chat.id, ctx.msgId);
+bot.command("new_message", async (ctx) => {
+  await ctx.reply("Qaysi turda xabar yubormoqchisiz? 👇", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: "🛡 Anonim", callback_data: "anonim" },
+          { text: "👀 Anonim emas", callback_data: "simple" },
+        ],
+      ],
+      one_time_keyboard: true,
+      resize_keyboard: true,
+    },
+  });
 });
 
-bot.hears("daily Umidxon", async ctx => {
-	const channelId = -1001897939296;
-	const userId = ctx.from.id;
-	await bot.telegram
-		.getChatMember(channelId, userId)
-		.then(async member => {
-			if (
-				member.status == "creator" ||
-				member.status == "administrator" ||
-				member.status == "member"
-			) {
-				await ctx.reply(
-					`<b>Daily | Umidxon</b> kanaliga qo'shilish uchun havola:\n👉 https://t.me/+--6VdTGW8cxlYjdi`,
-					{ parse_mode: "HTML", protect_content: true }
-				);
-			} else {
-				await ctx.reply(
-					"Afsuski siz @Umidxon_blog kanaliga obuna bo'lmagan ekansiz. Avval kanalga obuna bo'ling!"
-				);
-			}
-		})
-		.catch(err => ctx.reply("Noma'lum xatolik yuzaga keldi, qayta urining"));
+bot.action("anonim", async (ctx) => {
+  await ctx.scene.enter("anonimScene");
+  ctx.answerCbQuery("Iltimos faqat matnli xabar yuboring");
+  setTimeout(async () => {
+    await ctx.telegram.deleteMessage(ctx.chat.id, ctx.msgId);
+  }, 300);
 });
-bot.hears("Daily Umidxon", async ctx => {
-	const channelId = -1001897939296;
-	const userId = ctx.from.id;
-	await bot.telegram
-		.getChatMember(channelId, userId)
-		.then(async member => {
-			if (
-				member.status == "creator" ||
-				member.status == "administrator" ||
-				member.status == "member"
-			) {
-				await ctx.reply(
-					`<b>Daily | Umidxon</b> kanaliga qo'shilish uchun havola:\n👉 https://t.me/+--6VdTGW8cxlYjdi`,
-					{ parse_mode: "HTML", protect_content: true }
-				);
-			} else {
-				await ctx.reply(
-					"Afsuski siz @Umidxon_blog kanaliga obuna bo'lmagan ekansiz. Avval kanalga obuna bo'ling!"
-				);
-			}
-		})
-		.catch(err => ctx.reply("Noma'lum xatolik yuzaga keldi, qayta urining"));
+bot.action("simple", async (ctx) => {
+  await ctx.scene.enter("senderScene");
+  ctx.answerCbQuery("Iltimos faqat matnli xabar yuboring");
+  setTimeout(async () => {
+    await ctx.telegram.deleteMessage(ctx.chat.id, ctx.msgId);
+  }, 100);
 });
 
-bot.on("text", async ctx => {
-	await ctx.reply(
-		"Menimcha noto'g'ri buyruq yubordingiz 🤷‍♂️, tekshirib qaytadan urining"
-	);
+bot.action("cencel", async (ctx) => {
+  ctx.reply(
+    "<b>Muvaffaqiyatli bekor qilindi</b>\nYangi xabar yuborish uchun <b>/new_message</b> buyrug'idan foydalanishingiz mumkin",
+    { parse_mode: "HTML" }
+  );
+  await ctx.scene.leave();
+  ctx.answerCbQuery("Bekor qilindi ✅");
+  await ctx.telegram.deleteMessage(ctx.chat.id, ctx.msgId);
 });
-bot.on("message", async ctx => {
-	await ctx.reply("Faqat matnli xabar qabul qilinadi");
+
+bot.hears("daily Umidxon", async (ctx) => {
+  const channelId = -1001897939296;
+  const userId = ctx.from.id;
+  await bot.telegram
+    .getChatMember(channelId, userId)
+    .then(async (member) => {
+      if (
+        member.status == "creator" ||
+        member.status == "administrator" ||
+        member.status == "member"
+      ) {
+        await ctx.reply(
+          `<b>Daily | Umidxon</b> kanaliga qo'shilish uchun havola:\n👉 https://t.me/+--6VdTGW8cxlYjdi`,
+          { parse_mode: "HTML", protect_content: true }
+        );
+      } else {
+        await ctx.reply(
+          "Afsuski siz @Umidxon_blog kanaliga obuna bo'lmagan ekansiz. Avval kanalga obuna bo'ling!"
+        );
+      }
+    })
+    .catch((err) => ctx.reply("Noma'lum xatolik yuzaga keldi, qayta urining"));
+});
+
+bot.hears("Daily Umidxon", async (ctx) => {
+  const channelId = -1001897939296;
+  const userId = ctx.from.id;
+  await bot.telegram
+    .getChatMember(channelId, userId)
+    .then(async (member) => {
+      if (
+        member.status == "creator" ||
+        member.status == "administrator" ||
+        member.status == "member"
+      ) {
+        await ctx.reply(
+          `<b>Daily | Umidxon</b> kanaliga qo'shilish uchun havola:\n👉 https://t.me/+--6VdTGW8cxlYjdi`,
+          { parse_mode: "HTML", protect_content: true }
+        );
+      } else {
+        await ctx.reply(
+          "Afsuski siz @Umidxon_blog kanaliga obuna bo'lmagan ekansiz. Avval kanalga obuna bo'ling!"
+        );
+      }
+    })
+    .catch((err) => ctx.reply("Noma'lum xatolik yuzaga keldi, qayta urining"));
+});
+
+bot.on("text", async (ctx) => {
+  await ctx.reply(
+    "Menimcha noto'g'ri buyruq yubordingiz 🤷‍♂️, tekshirib qaytadan urining"
+  );
+});
+bot.on("message", async (ctx) => {
+  await ctx.reply("Faqat matnli xabar qabul qilinadi");
 });
 
 bot.launch(() => {
-	console.log(`Bot started!`);
+  console.log(`Bot started!`);
 });
 
 module.exports = bot;
--- src/core/scenes/senderScene.js
+++ src/core/scenes/senderScene.js
@@ -1,45 +1,48 @@
 const { Scenes } = require("telegraf");
 
 const senderScene = new Scenes.BaseScene("senderScene");
-senderScene.enter(ctx =>
-	ctx.reply(
-		`Matnli xabar yuboring\nXabar kim tomonidan yuborilgani @umidxon_polatxonov'ga ko'rinadi ⚠️`,
-		{
-			parse_mode: "HTML",
-			reply_markup: {
-				inline_keyboard: [
-					[{ text: "Bekor qilish ❌", callback_data: "cencel" }],
-				],
-			},
-		}
-	)
+senderScene.enter((ctx) =>
+  ctx.reply(
+    `Matnli xabar yuboring\nXabar kim tomonidan yuborilgani @umidxon_polatxonov'ga ko'rinadi ⚠️`,
+    {
+      parse_mode: "HTML",
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Bekor qilish ❌", callback_data: "cencel" }],
+        ],
+      },
+    }
+  )
 );
 
-senderScene.on("text", ctx => {
-	if (ctx.msg.text.startsWith("/")) {
-		ctx.reply("Kechirasiz, bot buyruqlarini yuborish imkonsiz");
-	} else {
-		ctx.reply(
-			`<b>Xabaringiz muvaffaqiyatli yuborildi ✅</b>\nYana xabar yuborish uchun /new_message buyru'gidan foydalaning`,
-			{ parse_mode: "HTML" }
-		);
-		ctx.telegram
-			.sendMessage(
-				5511267540,
-				`📨 <b>Sizda yangi xabar bor</b>\n\n👤 Ism: <a href="tg://user?id=${ctx.from.id}" >${ctx.from.first_name}</a>\n🆔 Chat ID: <code>${ctx.from.id}</code>\n💬 Xabar 👉 <i>${ctx.msg.text}</i>`,
-				{ parse_mode: "HTML" }
-			)
-			.catch(err =>
-				ctx.reply(
-					"Xabar yuborishda xatolik yuzaga keldi ❌\nIltimos qayta urining"
-				)
-			);
-		return ctx.scene.leave();
-	}
+senderScene.on("text", (ctx) => {
+  const chatId = ctx.from.id;
+  if (ctx.msg.text.startsWith("/")) {
+    ctx.reply("Kechirasiz, bot buyruqlarini yuborish imkonsiz");
+  } else {
+    ctx.reply(
+      `<b>Xabaringiz muvaffaqiyatli yuborildi ✅</b>\nYana xabar yuborish uchun /new_message buyru'gidan foydalaning`,
+      { parse_mode: "HTML" }
+    );
+    setTimeout(() => {
+      ctx.telegram
+        .sendMessage(
+          5511267540,
+          `📨 <b>Sizda yangi xabar bor</b>\n\n👤 Ism: <a href="tg://user?id=${chatId}" >${ctx.from.first_name}</a>\n🆔 Chat ID: <code>${ctx.from.id}</code>\n🔗 Username: ${ctx.from.username === undefined ? "Username not set" : "@" + ctx.from.username}\n💬 Xabar 👉 <i>${ctx.msg.text}</i>`,
+          { parse_mode: "HTML" }
+        )
+        .catch((err) =>
+          ctx.reply(
+            "Xabar yuborishda xatolik yuzaga keldi ❌\nIltimos qayta urining"
+          )
+        );
+    }, 100);
+    return ctx.scene.leave();
+  }
 });
 
-senderScene.on("message", ctx => {
-	ctx.reply("Iltimos faqat matnli xabar yuboring!");
+senderScene.on("message", (ctx) => {
+  ctx.reply("Iltimos faqat matnli xabar yuboring!");
 });
 
 module.exports = senderScene;
